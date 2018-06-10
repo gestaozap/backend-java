376c0eb0d25489dcfd93284aa6dffd83887e3246
mcatossi <mcatossi@br.ibm.com>
2018-06-09 21:15:03 -0300

    add cloudant with errors

---
+++ src/main/java/br/com/ciabfebraban/model/Sale.java
@@ -0,0 +1,66 @@
+package br.com.ciabfebraban.model;
+
+public class Sale {
+
+	private String _id;
+	private String _rev;
+	private String value;
+	private String description;
+	private String date;
+	private String type;
+	
+	public Sale() {
+		this.type = "SALE";
+	}
+
+	public String get_id() {
+		return _id;
+	}
+
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+
+	public String get_rev() {
+		return _rev;
+	}
+
+	public void set_rev(String _rev) {
+		this._rev = _rev;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+	
+}
--- src/main/java/br/com/ciabfebraban/ws/SaleWS.java
+++ src/main/java/br/com/ciabfebraban/ws/SaleWS.java
@@ -1,21 +1,45 @@
 package br.com.ciabfebraban.ws;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import com.cloudant.client.api.Database;
+
+import br.com.ciabfebraban.conn.MyUtils;
+import br.com.ciabfebraban.model.Sale;
+
+
 
 @Path("/sale")
 public class SaleWS {
 
+
+	@Context HttpServletRequest request;
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 //	@Path("/{id}")
 //	@PathParam("id") String id
 	public Response getEmploymentByID() {
-		return Response.status(200).entity("show").build();
+		return Response.status(200).entity("{\"response\": \"show\"}").build();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response insertSale(Sale sale) {
+		Database conn = MyUtils.getStoredConnection(request);
+		com.cloudant.client.api.model.Response response = conn.save(sale);
+	
+		System.out.println("_id" + response.getId());
+		
+		return Response.status(200).entity("{\"response\": \"ok\"}").build();
 	}
 	
 }
