c3cdbad30a8ac8bc77b10c870170975ffd6f43fd
mcatossi <mcatossi@br.ibm.com>
2018-06-09 21:25:33 -0300

    add cloudant with errors

--- src/main/java/br/com/ciabfebraban/conn/CloudantClientMgr.java
+++ src/main/java/br/com/ciabfebraban/conn/CloudantClientMgr.java
@@ -12,8 +12,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-
-
 public class CloudantClientMgr {
 
 	private static CloudantClient cloudant = null;
